9c52b49d261045e8795cc50782dcabd310b5f4c6
Jiajia Chen <audreychenjj@outlook.com>
2019-10-08 22:00:57 -0700

    project 1 first commit

---
+++ p1/README.md
@@ -0,0 +1,9 @@
+# Project 1
++ By: Jiajia Chen
++ Product URL: <http://p1.acjj.me>
+
+## Outside resources
+None
+
+## Notes for instructor
+None
---
+++ p1/script.js
@@ -0,0 +1,72 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+        playerName: '',
+        gameMode: 'start',
+        submitted: false,
+        level: '',
+        correct: false,
+        feedbackClass: '',
+        valid: true,
+        feedback: '',
+        solution: null,
+        guess: '',
+        guesses: [],
+    },
+    methods: {
+        // solutionGenerator: function () {
+        //     this.solution = Math.floor(Math.random() * 10) + 1
+        // },
+        startGame: function () {
+            this.gameMode = 'play';
+            this.loadGame();
+        },
+
+        solutionGenerator: function () {
+            if (this.level == 'easy') {
+                this.solution = Math.floor(Math.random() * 10) + 1;
+            }
+            else if (this.level == 'moderate') {
+                this.solution = Math.floor(Math.random() * 25) + 1;
+            }
+            else if (this.level == 'difficult') {
+                this.solution = Math.floor(Math.random() * 50) + 1;
+            }
+            else {
+                this.solution = null;
+            }
+        },
+
+        submitGuess: function () {
+            this.submitted = true;
+            this.valid = true;
+            // this.guesses.push(this.guess);
+
+            if (typeof (this.guess) != 'number') {
+                this.feedbackClass = 'validationError';
+                this.valid = false;
+                this.submitted = false;
+                this.feedback = 'Enter a valid number';
+            } else if (this.guess > this.solution) {
+                this.feedbackClass = 'error';
+                this.feedback = 'Too high. Try again.';
+                this.correct = false;
+            } else if (this.guess < this.solution) {
+                this.feedbackClass = 'error';
+                this.feedback = 'Too low. Try again.';
+                this.correct = false;
+            } else {
+                this.feedbackClass = '';
+                this.feedback = 'You got it!';
+                this.correct = true;
+            }
+        },
+
+        loadGame: function () {
+            this.feedback = '';
+            this.guess = '';
+            this.solutionGenerator();
+        }
+    },
+
+});
